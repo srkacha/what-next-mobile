1d84a89f90ee3fc8947d6466cc1aa02de54bc8e0
sjovic <Srdjan.Jovic@rt-rk.com>
2019-10-28 16:29:15 +0100

    Create search bar

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button, TextInput } from 'react-native';
+import {Icon, Card } from 'react-native-elements';
+import { Autocomplete, withKeyboardAwareScrollView } from 'react-native-dropdown-autocomplete'
 
 export default function App() {
+  const data = [
+    "Jova",
+    "pero",
+    "movie"
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header} id="header">
+        <Text style = {styles.title}>What next?</Text>
+        <View style = {styles.searchSection}>
+          <Autocomplete data = {data} valueExtractor = {item => item} highLightColor="white" spinnerColor="white" placeholder="Enter a movie" style = {styles.input}></Autocomplete>
+          <Icon style={{padding: 10}} name='search' size={35} color='#2e2e2e'></Icon>
+        </View>
+
+        <Button color="#fcba03" title="Suggest me some movies"></Button>
+      </View>
+      <ScrollView>
+
+      </ScrollView>
     </View>
   );
 }
@@ -13,7 +32,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header:{
+    backgroundColor: '#2e2e2e',
+    padding: 20,
+  },
+  input:{
+    width: '85%',
+    backgroundColor: 'white',
+    fontSize: 30,
+    padding: 5,
+    borderRadius: 5,
+    marginBottom:5,
+    borderWidth:0
+  },
+  title:{
+    color:'white',
+    color: '#d90000',
+    fontSize: 40,
+    textAlign: 'center',
+    fontFamily:'arial',
+    fontWeight:'bold'
+  },
+   searchSection:{
+     flexDirection: 'row',
+     alignItems: 'center',
+     backgroundColor:'white',
+     borderRadius: 5,
+     marginBottom: 5
+   }
 });
