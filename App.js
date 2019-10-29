9f6e2a2557f0e0d1b2fe09528dc97b25bd2a9e33
sjovic <Srdjan.Jovic@rt-rk.com>
2019-10-29 10:10:21 +0100

    Create search suggestions, update app look

--- App.js
+++ App.js
@@ -1,31 +1,113 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, Button, TextInput } from 'react-native';
 import {Icon, Card } from 'react-native-elements';
-import { Autocomplete, withKeyboardAwareScrollView } from 'react-native-dropdown-autocomplete'
 
-export default function App() {
-  const data = [
-    "Jova",
-    "pero",
-    "movie"
-  ]
+var suggestBaseURL = 'https://what-next-app.herokuapp.com/what-next/api/movies/auto-suggest/';
+var recommendBaseURL = 'https://what-next-app.herokuapp.com/what-next/api/movies/recommend/';
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header} id="header">
-        <Text style = {styles.title}>What next?</Text>
-        <View style = {styles.searchSection}>
-          <Autocomplete data = {data} valueExtractor = {item => item} highLightColor="white" spinnerColor="white" placeholder="Enter a movie" style = {styles.input}></Autocomplete>
-          <Icon style={{padding: 10}} name='search' size={35} color='#2e2e2e'></Icon>
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.onInputChageHandler = this.onInputChageHandler.bind(this);
+    this.fetchSuggestionData = this.fetchSuggestionData.bind(this);
+    this.onButtonPressHandler = this.onButtonPressHandler.bind(this);
+    this.onSuggestionPressHandler = this.onSuggestionPressHandler.bind(this);
+  }
+
+  //State object
+  state = {
+    suggestions: [],
+    inputText: '',
+    selectedId: ''
+  };
+
+  fetchSuggestionData(){
+    let substring = this.state.inputText;
+    let url = suggestBaseURL + substring;
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((responseJson) => {
+      this.setState({
+        suggestions: responseJson.data
+      });
+    }).catch((error) => {
+      //Do something to handle errors
+    })
+  }
+
+  onInputChageHandler(text){
+    this.setState({
+      inputText: text
+    }, () => {
+      this.fetchSuggestionData();
+      if(this.state.inputText.length == 0){
+        this.setState({
+          suggestions: []
+        })
+      }
+    });
+  }
+
+  onButtonPressHandler(movie){
+    this.setState({
+      inputText: movie.title,
+      selectedId: movie.id,
+      suggestions:[]
+    })
+  }
+
+  onSuggestionPressHandler(movie){
+    this.setState({
+      inputText: movie.title,
+      selectedId: movie.id,
+      suggestions:[]
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header} id="header">
+          <View style={styles.titleSection}>
+              <View><Text style = {styles.title}>What </Text></View>
+              <View><Icon style={{padding: 10}} name="movie" color="#d90000" size={45}></Icon></View>
+              <View><Text style = {styles.title}> next?</Text></View>
+          </View>
+          <View style = {styles.searchSection}>
+            <TextInput value = {this.state.inputText} onChangeText = {this.onInputChageHandler} placeholder="Enter a movie" style = {styles.input}></TextInput>
+            <Icon style={{padding: 10}} name='search' size={35} color='#2e2e2e'></Icon>
+          </View>
+          <ScrollView style={styles.suggestions}>
+              {this.state.suggestions.map(movie => (
+                <View key = {movie.id}>
+                  <Text onPress = {() => {this.onSuggestionPressHandler(movie)}}>{movie.title}</Text>
+                </View>
+              ))}
+            </ScrollView>
+          <Button onPress = {this.onButtonPressHandler} color="#fcba03" title="Suggest me some movies"></Button>
         </View>
-
-        <Button color="#fcba03" title="Suggest me some movies"></Button>
+        <ScrollView>
+          <Text style={{fontSize:50, textAlign:'center'}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+          <Text style={{fontSize:50}}>sdfsdfs</Text>
+        </ScrollView>
       </View>
-      <ScrollView>
+    );
+  }
 
-      </ScrollView>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
@@ -36,9 +118,10 @@ const styles = StyleSheet.create({
   header:{
     backgroundColor: '#2e2e2e',
     padding: 20,
+    paddingTop:40
   },
   input:{
-    width: '85%',
+    width: '90%',
     backgroundColor: 'white',
     fontSize: 30,
     padding: 5,
@@ -46,19 +129,33 @@ const styles = StyleSheet.create({
     marginBottom:5,
     borderWidth:0
   },
+  titleSection:{
+    flexDirection:'row',
+    textAlign:'center',
+    alignItems: 'center',
+  },
   title:{
     color:'white',
-    color: '#d90000',
+    color: 'red',
     fontSize: 40,
     textAlign: 'center',
-    fontFamily:'arial',
+    //fontFamily:'serif',
     fontWeight:'bold'
   },
    searchSection:{
      flexDirection: 'row',
      alignItems: 'center',
      backgroundColor:'white',
-     borderRadius: 5,
-     marginBottom: 5
+     borderTopLeftRadius: 5,
+     borderTopRightRadius: 5,
+     marginBottom: 0
+   },
+   suggestions:{
+     backgroundColor:'white',
+     marginBottom:5,
+     paddingHorizontal: 5,
+     paddingBottom: 5,
+     borderBottomLeftRadius: 5,
+     borderBottomRightRadius: 5
    }
 });
